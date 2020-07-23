c8ea0ddc6f1b102ab6148a7b8ac3d9e4214034fa
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-07-22 20:35:35 -0700

    added AWS Amplify

---
+++ src/graphql/queries.js
@@ -0,0 +1,30 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getNotes = /* GraphQL */ `
+  query GetNotes($id: ID!) {
+    getNotes(id: $id) {
+      id
+      note
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listNotess = /* GraphQL */ `
+  query ListNotess(
+    $filter: ModelNotesFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listNotess(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        note
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateNotes = /* GraphQL */ `
+  subscription OnCreateNotes {
+    onCreateNotes {
+      id
+      note
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateNotes = /* GraphQL */ `
+  subscription OnUpdateNotes {
+    onUpdateNotes {
+      id
+      note
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteNotes = /* GraphQL */ `
+  subscription OnDeleteNotes {
+    onDeleteNotes {
+      id
+      note
+      createdAt
+      updatedAt
+    }
+  }
+`;
