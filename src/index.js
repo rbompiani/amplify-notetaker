4603542aa43e5336c6e3bf5363aa29c2bfd806de
Rebecca Bompiani <rebeccabompiani@gmail.com>
2020-08-01 16:43:39 -0700

    added aws authentication sign in

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+//package imports
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,6 +13,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
